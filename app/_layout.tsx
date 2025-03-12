023c25db74dda7846f60aaeaf636f1542de8ae71
Dins2344 <dinson.cd@gmail.com>
2025-03-13 00:05:44 +0530

    added strapi api auth , fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [font, setFont] = useFonts({
+    roboto: require("../assets/fonts/Roboto-Regular.ttf"),
+    "roboto-bold": require("../assets/fonts/Roboto-Bold.ttf"),
+  });
+  return (
+    <Stack>
+      <Stack.Screen
+        name="Landing"
+        options={{
+          headerShown: false,
+        }}
+      ></Stack.Screen>
+    </Stack>
+  );
 }
